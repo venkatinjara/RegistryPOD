dfa4a845255fee64107a410088854e3faf75b2a5
Venkateswar Rao I <venkateswarrao.i@photon.in>
2022-04-07 15:32:09 +0530

    ======

---
+++ Views/FirstViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.swift
+//  RegistryPOD
+//
+//  Created by Venkateswar Rao I on 07/04/22.
+//
+
+import Foundation
+public class FirstViewController {
+    private init(){}
+    public static func getRepoValues() -> String{
+        
+        return "First Launch Repo"
+    }
+}
